360d2a0be50555c2d6d08f750a0c54c0ccdbd623
jangdemrunal4-cloud <jangdemrunal4@gmail.com>
2026-07-27 12:59:07 +0530

    Add files via upload

---
+++ MrunalGPT.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaLLM
+#prompt template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"), 
+        ("user","Question:{question}")
+    ]
+)
+# streamlit template
+st.title("My GPT")
+input_text=st.text_input("Ask me a question?")
+llm = OllamaLLM(model="gemma2:2b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+st.write(chain.invoke({"question":input_text}))
